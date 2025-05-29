17d8bc9879136eacadc7999254ae26d19387de56
Adam Mitchell <adam.mitchell@brillpower.com>
2025-05-29 10:50:21 +0000

    Implement ThingSetTextEncoder tests

--- tests/test_text_encoder.py
+++
@@ -1,9 +0,0 @@
-from python_thingset.encoders import ThingSetTextEncoder
-
-
-encoder = ThingSetTextEncoder()
-
-
-def test_root():
-    encoded = encoder.encode_get("")
-    assert encoded == "thingset ?\n".encode()
